ab20e25ed36d1d46d48089cc8bdfc2dfce44b6b0
dai-guu <89032169+dai-guu@users.noreply.github.com>
2022-02-22 18:17:00 +0900

    Initial Commit

---
+++ sample1/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  sample1
+//
+//  Created by おがわひさと on 2022/02/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    var myAge = 23
+    var pi = 3.14
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+//        myAge = 24
+//        print(myAge)
+          pi = 5
+          print(pi)
+    }
+
+
+}
+
